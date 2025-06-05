9daacaa92d1730f3476b91d0343c01d9dd11866a
Rahul Naskar <rahul9875458880@gmail.com>
2025-06-05 18:13:43 +0530

    having problem with deploying monorepo through vm...cannot find the @prisma/client cannot build the whole repo bcoz of prisma client had to remove the imports from everywhere

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { router } from "./routes";
+// import { router } from "./routes";
 
 const app = express();
 
 
 app.use(express.json());
-app.use("/api/v1", router);
+// app.use("/api/v1", router);
 
 
 function startServer() {
