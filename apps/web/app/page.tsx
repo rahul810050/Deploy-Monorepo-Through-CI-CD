b4550f5bc844147d0a89ddbed8da5d85eb00d0b3
Rahul Naskar <rahul9875458880@gmail.com>
2025-06-06 22:53:32 +0530

    fixed everything now it should be deployed

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,9 @@
-import prisma from "@repo/db/client";
 export default async function Home() {
-  const res = await prisma.user.findFirst();
   return (
     <div>
       <h1>Hello World</h1>
-      <p>{res?.id}</p>
-      <p>{res?.username}</p>
-      <p>{res?.password}</p>
+      <p>name: Rahul</p>
+      <p>username: cocane</p>
     </div>
   );
 }
